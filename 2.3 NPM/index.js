4b726bea9b0f67bf45599a3111a9fc8ac559af38
Awais Shabbir <150779796+AwaisShabbir0@users.noreply.github.com>
2025-07-26 23:19:28 +0500

    Using npm packages
    work on random name generator or super hero name generator npm package.

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// //var generateName = require('sillyname');
+// import generateName from 'sillyname';
+// var sillyName = generateName();
+// console.log(`my name is ${sillyName}`);
+// //console.log('my name is '+ generateName());
+
+import {randomSuperhero} from 'superheroes';
+var superheroName = randomSuperhero();
+console.log(`my superhero name is ${superheroName}`);
